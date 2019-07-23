b79939d5a7687ffefd97936c0448faf60e54ea10
Hector Zelaya <inge.zelaya@gmail.com>
2019-07-23 17:18:41 -0600

    aded redirection after login/logout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from "react-router-bootstrap";
 import Routes from "./Routes";
+import {Auth} from "aws-amplify"
 import './App.css';
 
 class App extends Component {
@@ -10,16 +11,32 @@ class App extends Component {
     super(props);
 
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      isAuthenticating: true
     }
+  }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    } catch(e) {
+      if(e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    this.setState({isAuthenticating: false});
   }
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated })
   }
 
-  handleLogout = event => {
+  handleLogout = async event => {
+    await Auth.signOut();
     this.userHasAuthenticated(false)
+    this.props.history.push("/login")
   }
 
   render() {
@@ -28,6 +45,7 @@ class App extends Component {
       userHasAuthenticated: this.userHasAuthenticated
     }
     return (
+      !this.state.isAuthenticating &&
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
@@ -58,4 +76,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
