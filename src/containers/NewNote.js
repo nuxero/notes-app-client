cf3acbef151c6fbdf13718ba8a14251b9a555386
Hector Zelaya <inge.zelaya@gmail.com>
2019-07-25 17:13:37 -0600

    added pages for creating and listing notes

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -2,7 +2,9 @@ import React, {Component} from "react";
 import {FormGroup, FormControl, ControlLabel} from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
+import {API} from "aws-amplify";
 import "./NewNote.css";
+import { s3Upload } from "../libs/awsLib";
 
 export default class NewNote extends Component {
     constructor(props) {
@@ -39,6 +41,27 @@ export default class NewNote extends Component {
         }
 
         this.setState({isLoading: true});
+
+        try {
+            const attachment = this.file
+                ? await s3Upload(this.file)
+                : null;
+
+            await this.createNote({
+                attachment,
+                content: this.state.content
+            });
+            this.props.history.push("/");
+        } catch (e) {
+            alert(e);
+            this.setState({isLoading: false});
+        }
+    }
+
+    createNote(note) {
+        return API.post("notes", "/notes", {
+            body: note
+        })
     }
 
     render() {
